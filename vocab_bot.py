97f96711347af0ca9ef0df7d4a3ce9c94513f649
shiva <skchintu000@gmail.com>
2025-07-19 17:27:59 +0530

    removed webhook url

--- vocab_bot.py
+++ vocab_bot.py
@@ -12,7 +12,6 @@ MONGO_URI = os.getenv("MONGO_URI")
 CHAT_ID = os.getenv("CHAT_ID")
 APP_URL = os.getenv("APP_URL")
 PORT = int(os.environ.get("PORT", "10000"))
-WEBHOOK_URL = "https://daily-vocabulary-bot.onrender.com/vocab-secret-123"  # Replace with your webhook path
 
 client = MongoClient(MONGO_URI)
 db = client.vocab_bot
@@ -102,12 +101,12 @@ def main():
         time=datetime.time(hour=8, minute=0),
     )
 
-    # Webhook start
     app.run_webhook(
-    listen="0.0.0.0",
-    port=PORT,
-    webhook_url=f"{APP_URL}/{BOT_TOKEN}"
+        listen="0.0.0.0",
+        port=PORT,
+        webhook_path=f"/{BOT_TOKEN}"  # This is IMPORTANT!
     )
+
 
 if __name__ == "__main__":
     main()
