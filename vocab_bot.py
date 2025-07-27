4c547f6544c96ee98d6cd8ecfb513313b01d00cd
shiva <skchintu000@gmail.com>
2025-07-27 18:01:29 +0530

    changed code

--- vocab_bot.py
+++ vocab_bot.py
@@ -144,4 +144,9 @@ async def main():
         raise
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(main())
+    finally:
+        loop.run_until_complete(loop.shutdown_asyncgens())
+        loop.close()
